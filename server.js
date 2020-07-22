5633f55fb0ece3769233015919eab041257bb0e6
CamilleSylla <camillesylla.web@gmail.com>
2020-07-22 19:09:56 +0200

    a ajouté une methode get et post (creer une erreur sur la methode popst)

--- server.js
+++ server.js
@@ -9,10 +9,24 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: false}));
 
 app.get('/todos', (req, res) => {
-    knex.raw('select * from todos').then(function (todos) {
+    knex.select().from('todos')
+    .then(function (todos){
         res.send(todos)
     })
-  });
+  })
+
+  app.post('/todos', (req, res) => {
+    knex('todos').insert({
+        title: "go play some shorcuts",
+        user_id: 1
+    }).then(function (){
+        knex.select().from('todos')
+        .then(function (todos){
+            res.send(todos)
+        })
+    })
+  })
+
   
   app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
