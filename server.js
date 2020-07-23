e29d995e7bea35f8e6b99b789e0115c4cc7a7be0
CamilleSylla <camillesylla.web@gmail.com>
2020-07-23 14:33:53 +0200

    add delete upload put methode and all works

--- server.js
+++ server.js
@@ -15,15 +15,66 @@ app.get('/todos', (req, res) => {
     })
   })
 
+app.get('/todos/:id', function(req, res) {
+  knex.select()
+    .from('todos')
+    .where('id', req.params.id)
+    .then(function(todos){
+      res.send(todos);
+    })
+})
+
   app.post('/todos', (req, res) => {
     knex('todos').insert({
-        title: "go play some shorcuts",
-        user_id: 1
-    }).then(function (){
+        title: req.body.title,
+        user_id: req.body.user_id
+    })
+    .then(function (){
         knex.select().from('todos')
         .then(function (todos){
             res.send(todos)
         })
+    })
+  })
+
+  app.put('/todos/:id', function(req, res) {
+    knex('todos')
+    .where('id', req.params.id)
+    .update({
+    title: req.body.title,
+    completed: req.body.completed
+    })
+    .then(function(){
+      knex.select()
+      .from('todos')
+      .then(function(todos){
+        res.send(todos)
+      })
+      
+    })
+  })
+
+  app.delete('/todos/:id', function(req, res){
+    knex('todos')
+    .where('id', req.params.id)
+    .del()
+    .then(function(){
+      knex.select()
+      .from('todos')
+      .then(function(todos){
+        res.send(todos)
+      })
+    })
+  })
+
+  //////////////////////////////////Join
+
+  app.get('/todos-of-user/:id', function(req, res) {
+    knex.from('todos')
+    .innerJoin('users', 'todos.user_id', 'users.id')
+    .where('todos.user_id', req.params.id)
+    .then(function(data){
+      res.send(data)
     })
   })
 
